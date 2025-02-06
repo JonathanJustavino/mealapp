59f759fb00531c8d59e5e6436338805f4e474a30
Jonathan <jonathan.luederitz@gmx.de>
2025-02-06 17:04:22 +0100

    Add toggle like button

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideIcons } from '@ng-icons/core';
+import { bootstrapHeart, bootstrapHeartFill } from '@ng-icons/bootstrap-icons';
+
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideIcons({bootstrapHeart, bootstrapHeartFill}),
     provideHttpClient(),
-  ]
+    provideStore()
+]
 };
--- src/app/meal/meal.component.ts
+++ src/app/meal/meal.component.ts
@@ -1,23 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input } from '@angular/core';
 import { Meal } from '../data/meal';
+import { IconType, NgIconComponent } from '@ng-icons/core';
+import { bootstrapHeart, bootstrapHeartFill } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-meal',
-  imports: [],
+  imports: [
+    NgIconComponent,
+  ],
   templateUrl: './meal.component.html',
   styleUrl: './meal.component.css',
   providers: []
 })
 export class MealComponent {
+  readonly headingCharacterLimit = 28
+
+  thumbnail: string | undefined;
+  headingSize = "text-xl";
+  liked: boolean = true;
+  buttonIcon: IconType = bootstrapHeart;
+
   @Input() size!: Number;
   @Input() meal!: Meal;
-  thumbnail: string | undefined;
+
+
+  toggle() {
+    this.liked = !this.liked;
+    this.buttonIcon = !this.liked ? bootstrapHeartFill : bootstrapHeart;
+  }
 
   ngOnInit() {
     this.thumbnail = `http://localhost:3000/image/${this.meal.idMeal}`;
   }
 
   constructor() {
-
+    this.buttonIcon = bootstrapHeart;
   }
 }
