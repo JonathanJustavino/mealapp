21ecdaf4f5a79515033c3265ea6021aa16e34b4a
Jonathan L. <jonathan.luederitz@gmx.de>
2025-02-17 22:01:47 +0100

    Add observable likestatus property

--- src/app/meal/meal.component.ts
+++ src/app/meal/meal.component.ts
@@ -1,35 +1,47 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { computed } from '@angular/core';
 import { Meal } from '../../model/meal.model';
+import { select, Store } from '@ngrx/store';
+import { mealFeature } from '../../state/meals.feature';
+import { map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-meal',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './meal.component.html',
   styleUrl: './meal.component.css'
 })
 export class MealComponent {
   @Input() meal!: Meal;
-  liked: boolean;
-  likeBtnLabel: string = "like";
+  isLiked$: Observable<boolean>
+  liked: boolean = false;
+  likeBtnLabel$: Observable<string>;
   @Output() like = new EventEmitter<string>();
   @Output() dislike = new EventEmitter<string>();
   
-
   toggleLike() {
     if(this.liked) {
-      this.liked = false;
-      this.likeBtnLabel = "like";
       this.dislike.emit(this.meal.idMeal!);
-      return
+      return 
     }
-
-    this.liked = true;
-    this.likeBtnLabel = "dislike";
     this.like.emit(this.meal.idMeal!);
   }
 
-  constructor() {
-    this.liked = false;
+  constructor(private store: Store) { 
+    this.isLiked$ = this.store.select(mealFeature.selectLiked).pipe(
+      map((likedList) => likedList.includes(this.meal?.idMeal ?? ""))
+    );
+    this.likeBtnLabel$ = this.isLiked$.pipe(
+      map((likedStatus) => likedStatus ? "dislike" : "like")
+    );
+  }
+
+  ngOnInit() {
+    //TODO: subscribing in constructor is error prone, since input data could not 
+    this.isLiked$.subscribe((likedStatus) => {
+      this.liked = likedStatus
+    });
   }
 
 }
