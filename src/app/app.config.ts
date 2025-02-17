d520b5ee2b5b472716b7be313471689e1d6492d3
Jonathan L. <jonathan.luederitz@gmx.de>
2025-02-17 17:48:37 +0100

    Setup mealfeature for ngrx store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { mealFeature } from '../state/meals.feature';
 
 
 export const appConfig: ApplicationConfig = {
@@ -13,6 +15,9 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     //TODO: uncomment when feature ready
     // provideState(),
-    provideRouter(routes), provideStore()
-  ]
+    provideRouter(routes),
+    provideStore({}),
+    provideState(mealFeature),
+    provideStoreDevtools({ name: "Meal App", maxAge: 25, logOnly: false, trace: true })
+]
 };
