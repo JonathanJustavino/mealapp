5a4738fbdaa2c3d894eb4af98b4aaccd932429ad
Jonathan L. <jonathan.luederitz@gmx.de>
2025-02-17 14:57:21 +0100

    Add httpclient for service usage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideStore(),
+    //TODO: uncomment when feature ready
+    // provideState(),
+    provideRouter(routes), provideStore()
+  ]
 };
