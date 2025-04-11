e59fe59fe6128a418f06edf09b36c6bd9e8b88d1
Jonathan <jonathan.luederitz@gmx.de>
2025-04-11 20:54:56 +0200

    Switch homepage image

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
   title = "Welcome to the Meal App";
-  homeImgSrc = "homepage-image.jpg";
+  homeImgSrc = "homepage-img.png";
 }
